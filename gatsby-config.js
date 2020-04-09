70f77ff10fa68b8377f9f9615950c2e9839418c2
ricepat <patrice@sourplumbum.com>
2020-04-09 16:23:52 -0700

    fixed gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,14 +35,14 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `{__dirname}/static/assets`,
+        path: `${__dirname}/static/assets`,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `content`,
-        path: `{__dirname}/content`,
+        path: `${__dirname}/content`,
       },
     },
     `gatsby-transformer-remark`,
