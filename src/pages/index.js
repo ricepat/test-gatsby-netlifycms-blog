f42dfdd3063e3a61689d16dd1743b0b1eb24d63d
ricepat <patrice@sourplumbum.com>
2020-04-09 16:19:09 -0700

    pulled in home page data with graphql into gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,39 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+import { graphql } from "gatsby"
+
+const IndexPage = (props) => {
+  const data = props.data.allFile.edges[0].node.childMarkdownRemark.frontmatter
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>{data.title}</h1>
+      <p>{data.intro}</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <img src={data.image} alt="pic of pink flower" />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    allFile (filter: {sourceInstanceName: {eq: "content"} name: {eq: "home"}}) {
+      edges {
+        node {
+          childMarkdownRemark {
+            frontmatter {
+              title
+              intro
+              image
+            }
+          }
+        }
+      }
+    }
+  }`
